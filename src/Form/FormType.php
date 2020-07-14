7e7b162362acbf64276d05b061b8a25869cf1748
Galt3s <galt3s@gmail.com>
2020-07-14 17:48:48 +0200

    Add files via upload

---
+++ src/Form/FormType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Form;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('surname')
+            ->add('email')
+            ->add('phoneNumber')
+            ->add('save', SubmitType::class)
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Form::class,
+        ]);
+    }
+}
